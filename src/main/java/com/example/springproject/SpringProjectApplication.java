4eb3254e73500a414360a9ec28ce1476caa5aff4
Diego Matté <diegocmatte@gmail.com>
2021-09-21 13:37:41 -0300

    REST testado

--- src/main/java/com/example/springproject/SpringProjectApplication.java
+++ src/main/java/com/example/springproject/SpringProjectApplication.java
@@ -3,6 +3,7 @@ package com.example.springproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringProjectApplication {
 
