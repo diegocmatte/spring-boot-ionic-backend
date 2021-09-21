fe732b7bde20a23de428c58ad93a53c80ef564e4
Diego Matté <diegocmatte@gmail.com>
2021-09-21 19:54:59 -0300

    Banco de Dados H2 e criacao automatica da base de dados

--- src/main/java/com/example/springproject/resources/CategoriaResource.java
+++ src/main/java/com/example/springproject/resources/CategoriaResource.java
@@ -1,7 +1,6 @@
 package com.example.springproject.resources;
 
 import com.example.springproject.domain.Categoria;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
