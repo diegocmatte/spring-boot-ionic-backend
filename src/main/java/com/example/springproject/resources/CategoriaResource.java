a72ce9092f867e338b06aa0a286fa27ddb2ab823
Diego Matté <diegocmatte@gmail.com>
2021-09-29 14:28:05 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/example/springproject/resources/CategoriaResource.java
+++ src/main/java/com/example/springproject/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import com.example.springproject.domain.Categoria;
 import com.example.springproject.dto.CategoriaDTO;
 import com.example.springproject.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -44,7 +45,6 @@ public class CategoriaResource {
         obj.setId(id);
         obj = categoriaService.update(obj);
         return ResponseEntity.noContent().build();
-
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
@@ -59,7 +59,20 @@ public class CategoriaResource {
         List<Categoria> list = categoriaService.findAll();
         List<CategoriaDTO> listDTO = list.stream()
                                          .map(CategoriaDTO::new).collect(Collectors.toList());
-        return ResponseEntity.ok().body(listDTO);
 
+        return ResponseEntity.ok().body(listDTO);
     }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                       @RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage,
+                                                       @RequestParam(value = "orderBy", defaultValue = "nome")String orderBy,
+                                                       @RequestParam(value = "direction", defaultValue = "ASC")String direction){
+
+        Page<Categoria> list = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> listDTO = list.map(CategoriaDTO::new);
+
+        return ResponseEntity.ok().body(listDTO);
+    }
+
 }
