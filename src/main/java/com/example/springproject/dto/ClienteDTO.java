2af206be31c0010a050a5d61979101d1b3a04b42
Diego Matté <diegocmatte@gmail.com>
2021-10-06 15:40:36 -0300

    Validação customizada: email não repetido

--- src/main/java/com/example/springproject/dto/ClienteDTO.java
+++ src/main/java/com/example/springproject/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.example.springproject.dto;
 
 import com.example.springproject.domain.Cliente;
+import com.example.springproject.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
