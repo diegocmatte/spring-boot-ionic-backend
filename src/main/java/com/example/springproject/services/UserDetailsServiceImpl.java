96b29c75cbc583af940c021a955e1734cb14cde7
Diego Matté <diegocmatte@gmail.com>
2022-02-23 16:03:24 -0300

    implementando autenticacao e geracao do token

---
+++ src/main/java/com/example/springproject/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.springproject.services;
+
+import com.example.springproject.domain.Cliente;
+import com.example.springproject.repositories.ClienteRepository;
+import com.example.springproject.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Cliente cliente = clienteRepository.findByEmail(email);
+
+        if(cliente == null){
+            throw new UsernameNotFoundException(email);
+        }
+
+        return new UserSS(cliente.getId(), cliente.getEmail(), cliente.getSenha(), cliente.getPerfis());
+    }
+}
